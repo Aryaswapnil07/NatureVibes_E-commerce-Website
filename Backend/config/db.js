a844a31d04ec5cdf6630cde0ebb6f0896ef3f355
Aryaswapnil07 <achintyasingh48@gmail.com>
2026-02-26 16:23:59 +0530

    added server and connection to mongodb

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,15 +1,15 @@
-//  THIS FILE HELPS TO CONNECT MONGODB TO DATABASE 
+//  THIS FILE HELPS TO CONNECT MONGODB TO DATABASE
 
-const mongoose = require("mongoose");
-const connectDB = async() => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB is connected successfully ");
+import mongoose from "mongoose";
 
-    } catch (error) {
-        console.error("MongoDB Connection failed",error)
-        process.exit(1)
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/NatureVibes`);
+    console.log("MongoDB is connected successfully ");
+  } catch (error) {
+    console.error("MongoDB Connection failed", error);
+    process.exit(1);
+  }
 };
 
-module.exports = connectDB;
+export default connectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,27 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const connectDB = require("./config/db.js");
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/db.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/user.Route.js";
 
-const userRoutes = require("./routes/userRoutes.js");
-const productRoutes = require("./routes/productRoutes.js");
-
-dotenv.config();
+// APP CONFIG
 
 const app = express();
+const port = process.env.PORT || 4000;
+connectDB()
+connectCloudinary()
+// MIDDLEWARES
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 
-// Connect DB
-connectDB();
+// API ENDPOINTS
 
-const PORT = process.env.PORT || 3000;
+app.use("/api/users", userRouter);
 
-// Test route
 app.get("/", (req, res) => {
-  res.send("Welcome to NatureVibes API!");
+  res.send("API IS WORKING ");
 });
 
-// API Routes
-app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);   // ✅ FIXED
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on http://localhost:${PORT}`);
-});
+app.listen(port, () => console.log(`server is started on PORT : ` + port));
